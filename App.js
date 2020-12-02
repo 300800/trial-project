22d33c51d94ca0562c170cd299c5e3c3ed632f20
grace <gracechongbl@gmail.com>
2020-12-03 00:24:04 +0800

    added title and para

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Starting to work on React Native</Text>
+      <p>This is my independent Trial Project</p>
       <StatusBar style="auto" />
     </View>
   );
